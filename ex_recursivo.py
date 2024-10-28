fad2602f90a1fde7795dd7ceefe510d8d41d8328
Gustavo Baron <gustavoalecxander9@gmail.com>
2024-10-28 20:29:30 -0300

    adição
    Add exercicios 6 e 8

--- ex_recursivo.py
+++ ex_recursivo.py
@@ -1,4 +1,4 @@
-#1) Faça uma função recursiva que calcule e retorne o fatorial de um número inteiro N.
+'''#1) Faça uma função recursiva que calcule e retorne o fatorial de um número inteiro N.
 def fat(N):
     if N == 0 or N == 1:
         return 1
@@ -53,10 +53,30 @@ def soma(n):
         return n + soma(n-1)
 
 numero = int(input('informe um numero'))
-print('a soma é', soma(numero))
+print('a soma é', soma(numero))'''
 
 #6) Crie um programa em Python que contenha uma função recursiva que receba dois inteiros positivos k e n e calcule k. Utilize apenas multiplicações. 
 #O programa principal deve solicitar ao usuário os valores de k e n e imprimir o resultado da chamada da função.
+
+def potencia(k, n):
+    if n == 0:
+        return 1
+    return k * potencia(k, n - 1)
+
+def main():
+    k = int(input("Digite um número inteiro positivo k: "))
+    n = int(input("Digite um número inteiro positivo n: "))
+    
+    resultado = potencia(k, n)
+    print(f"{k} elevado a {n} é: {resultado}")
+
+if __name__ == "__main__":
+    main()
+
+#8) O máximo divisor comum dos inteiros x e y é o maior inteiro que é divisível por x e y.
+# Escreva uma função recursiva mdc em C, que retorna o máximo divisor comum de x e y.
+#O mdc de x e y é definido como segue: se y é igual a 0, então mdc(x,y) é x; caso
+#contrário, mdc(x,y) é mdc (y, x%y), onde % é o operador resto.
 
 
 
